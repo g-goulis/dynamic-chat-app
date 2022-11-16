a0371603f9b92be49d378c51ab0f047e608093e7
Gabriel Goulis <ninjagabe2003@gmail.com>
2022-11-16 01:02:35 -0600

    Using .then() and .catch() for downward flowing promises instead of inward flowing callbacks

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+mongoose.Promise = Promise
+
 const dbUrl = mongoConfig.dbUrl;
 
 var Message = mongoose.model('Message', {
@@ -28,19 +30,27 @@ app.get('/messages', (req, res) =>{
 })
 
 app.post('/messages', (req, res) =>{
-    var message = new Message(req.body)
+    const message = new Message(req.body);
 
-    message.save((err) => {
-        if(err){
-            res.sendStatus(500)
-        } else {
-            io.emit('message', req.body)
-            res.sendStatus(200)
-        }
+    message.save()
+    .then(() => {
+        console.log('Saved message')
+        return Message.findOne({message: 'badword'})
     })
-
-
+    .then(censored => {
+        if(censored) {
+            console.log('censored words found', censored)
+            return Message.remove({_id: censored.id})
+        }
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        res.sendStatus(500)
+        return console.error(err)
+    })
 })
+
 
 io.on('connection', (socket) => {
     console.log('A user connected')
